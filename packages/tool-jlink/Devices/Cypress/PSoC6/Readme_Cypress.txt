1f8de04fecba7d1e3bde3ba2a5d6696ff431ab18
Tenkai Kariya <tenkai.kariya@synapse.com>
2020-12-21 13:15:17 -0800

    deleted autopopulated files

--- packages/tool-jlink/Devices/Cypress/PSoC6/Readme_Cypress.txt
+++
@@ -1,2 +0,0 @@
-The Cypress devices support comes without any warranty and support from SEGGER Microcontroller GmbH. Support is provided via Cypress only.
-For support, please contact: cytechsupport@cypress.com
--- platforms/ststm32/examples/arduino-mxchip-azureiot/src/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. 
-
-// Interval time(ms) for sending message to IoT Hub
-#define INTERVAL 5000
-
-#define MESSAGE_MAX_LEN 256
-
-#define TEMPERATURE_ALERT 30
